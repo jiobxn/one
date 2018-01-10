f756688660a1ecefdb796dac8300b531a7cc7f95
JIOBXN <jiobxn@users.noreply.github.com>
2018-01-10 10:17:59 +0800

    Update tinyvpn.sh

--- Docker/systemd/tinyvpn.sh
+++ Docker/systemd/tinyvpn.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "$1" = 'tinyvpn' ]; then
 
+: ${TUN_DEV:=tun10}
 : ${IP_RANGE:=10.12.0}
 : ${VPN_PORT:=8000}
 : ${VPN_PASS:=2017126@Guo}
@@ -10,10 +11,10 @@ if [ "$1" = 'tinyvpn' ]; then
 if [ ! -f /usr/bin/tinyvpn ]; then
 	if [ $VPN_SERVER ]; then
 		echo "service ip: $IP_RANGE.1"
-		echo "tinyvpn_amd64 -c -r$VPN_SERVER:$VPN_PORT -f20:10 -k \"$VPN_PASS\" --sub-net $IP_RANGE.0" >/usr/bin/tinyvpn
+		echo "tinyvpn_amd64 -c -r$VPN_SERVER:$VPN_PORT -f20:10 -k \"$VPN_PASS\" --tun-dev $TUN_DEV --sub-net $IP_RANGE.0" >/usr/bin/tinyvpn
 	else
 		echo -e "service port: $VPN_PORT \npassword: $VPN_PASS"
-		echo "tinyvpn_amd64 -s -l0.0.0.0:$VPN_PORT -f20:10 -k \"$VPN_PASS\" --sub-net $IP_RANGE.0" >/usr/bin/tinyvpn
+		echo "tinyvpn_amd64 -s -l0.0.0.0:$VPN_PORT -f20:10 -k \"$VPN_PASS\" --tun-dev $TUN_DEV --sub-net $IP_RANGE.0" >/usr/bin/tinyvpn
 	fi
 
 	chmod +x /usr/bin/tinyvpn
@@ -25,7 +26,8 @@ else
 	echo -e "
 	Example:
 				docker run -d --restart always --privileged \\
-				-p 8000:8000/udp \
+				-p 8000:8000/udp \\
+				-p TUN_DEV=[tun10] \\
 				-e VPN_PORT=[80] \\
 				-e VPN_PASS=[2017126@Guo] \\
 				--name filemanager filemanager
