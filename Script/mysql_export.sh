f964c55969d43406f7b5ec40c63affe32f842f81
JIOBXN <jiobxn@users.noreply.github.com>
2018-06-27 22:06:46 +0800

    Create mysql_export.sh

---
+++ Script/mysql_export.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+user=root
+pass=123456
+host=localhost
+
+out() {
+  for i in $(mysql -u$user -p$pass -h$host -e "show databases;" |awk 'NR!=1{print $1}' |egrep -v "information_schema|performance_schema|mysql|sys"); do
+    mysqldump -u$user -p$pass -h$host -R --single-transaction "$i" > "$i".sql
+  done
+}
+
+in() {
+  for i in $(ls *.sql |awk -F. '{print $1}');do 
+    mysql -u$user -p$pass -h$host -e "CREATE DATABASE $i;"
+    mysql -u$user -p$pass -h$host --default-character-set=utf8 $i < $i.sql
+  done
+}
+
