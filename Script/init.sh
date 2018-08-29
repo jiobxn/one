f59e6221a58d0e01890f041f2e2979c24b69c324
JIOBXN <jiobxn@users.noreply.github.com>
2018-08-29 10:20:52 +0800

    Update init.sh

--- Script/init.sh
+++ Script/init.sh
@@ -5,7 +5,11 @@ if [ "$(grep -o -w "Red Hat" /etc/redhat-release)" ]; then
     yum-config-manager --enable rhui-REGION-rhel-server-extras
 fi
 
-yum -y install bash-completion vim aria2 axel wget openssl-devel bind-utils iptables-services iftop nethogs net-tools ntp mtr nmap tcpdump pciutils setroubleshoot setools make gcc-c++ autoconf automake unzip bzip2 zip mailx bc at expect telnet git lrzsz lsof bridge-utils dos2unix certbot nfs-utils 
+if [ "`hostname`" == "vultr.guest" ]; then
+    yum -y install bash-completion vim aria2 wget bind-utils iptables-services iftop nethogs net-tools ntp mtr nmap tcpdump unzip bzip2 zip mailx bc at expect telnet git lrzsz lsof bridge-utils dos2unix certbot setroubleshoot setools
+else
+    yum -y install bash-completion vim aria2 wget bind-utils iptables-services iftop nethogs net-tools ntp mtr nmap tcpdump unzip bzip2 zip mailx bc at expect telnet git lrzsz lsof bridge-utils dos2unix certbot axel openssl-devel pciutils setroubleshoot setools make gcc-c++ autoconf automake nfs-utils 
+fi
 
 systemctl disable NetworkManager firewalld
 \cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
@@ -41,6 +45,15 @@ if [ $(free |awk '$1=="Swap:"{print $2}') -eq 0 ]; then
     chmod 0600 /swapfile
     swapon /swapfile
     echo "$uuid    swap    defaults        0 0" >>/etc/fstab
+fi
+
+if [ `ulimit -n` -eq 1024 ]; then
+    echo -e "*          soft    nproc     512000\nroot       soft    nproc     unlimited" >/etc/security/limits.d/20-nproc.conf
+    echo -e "*      -   nofile   1048576\n*      -   nproc    524288" >>/etc/security/limits.conf
+    echo "kernel.pid_max=512000" >> /etc/sysctl.conf 
+    echo "fs.file-max=512000" >>/etc/sysctl.conf
+    ulimit -n 1048576
+    sysctl -p
 fi
 
 audit2allow -a -M mycertwatch
