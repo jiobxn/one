e7354f66175d6db3816bf089810b632b9c3e4136
JIOBXN <jiobxn@users.noreply.github.com>
2018-08-29 08:29:14 +0800

    Update htpass_add.sh

--- Script/htpass_add.sh
+++ Script/htpass_add.sh
@@ -5,8 +5,8 @@ N=$(grep -v ^# $F |grep -v ^$ |wc -l txt |awk '{print $1}')
 
 i=1
 while [ $i -le $N ];do
-  U=$(sed -n ''$i'p' txt |awk '{print $1}')
-  P=$(sed -n ''$i'p' txt |awk '{print $2}')
+  U=$(grep -v ^# $F |grep -v ^$ |sed -n ''$i'p' |awk '{print $1}')
+  P=$(grep -v ^# $F |grep -v ^$ |sed -n ''$i'p' |awk '{print $2}')
   echo "$U:$(openssl passwd -apr1 $P)"
   let  i++
 done
