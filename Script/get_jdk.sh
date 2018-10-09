f6919066e58df76729793e57356f2d50eac1aea5
JIOBXN <jiobxn@users.noreply.github.com>
2018-10-09 22:36:17 +0800

    Update get_jdk.sh

--- Script/get_jdk.sh
+++ Script/get_jdk.sh
@@ -13,8 +13,12 @@ fi
 
 echo "$jdk_v $jdk_t"
 
-jdk_d1=$(curl -s http://www.oracle.com/technetwork/java/javase/downloads/index.html |egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${jdk_v}-downloads-.+?\.html" |head -1) 
+wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" -N http://www.oracle.com/technetwork/java/javase/downloads/index.html -O "jdk-$jdk_v-$jdk_t.html"
 
-[ -n $jdk_d1 ] && jdk_d2=$(curl -s http://www.oracle.com/$jdk_d1 |egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/jdk-${jdk_v}.*(-|_)linux-(x64|x64_bin).${jdk_t}" |tail -1)
+jdk_d1=$(egrep -o "\/technetwork\/java/\javase\/downloads\/jdk${jdk_v}-downloads-.+?\.html" "jdk-$jdk_v-$jdk_t.html" |head -1) && \rm "jdk-$jdk_v-$jdk_t.html"
+
+[ -n $jdk_d1 ] && wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" -N http://www.oracle.com/$jdk_d1 -O "jdk-$jdk_v-$jdk_t.html"
+
+jdk_d2=$(egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[8-9](u[0-9]+|\+).*\/jdk-${jdk_v}.*(-|_)linux-(x64|x64_bin).${jdk_t}" "jdk-$jdk_v-$jdk_t.html" |tail -1) && \rm "jdk-$jdk_v-$jdk_t.html"
 
 [ -n $jdk_d2 ] && wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" -N $jdk_d2
