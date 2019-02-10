7f2999e7f8a05c9b715b4c80c0946a54370a2357
JIOBXN <jiobxn@users.noreply.github.com>
2019-02-11 00:48:15 +0800

    Update dnscrypt.sh

--- Docker/dnscrypt/dnscrypt.sh
+++ Docker/dnscrypt/dnscrypt.sh
@@ -4,25 +4,25 @@ set -e
 if [ "$1" = 'dnscrypt' ]; then
 
 : ${VERSION:="windows 2003 DNS"}
-: ${BIND_ADDR:="0.0.0.0"}
-: ${BIND_PORT:="53"}
+: ${LISTEN_ADDR:="0.0.0.0"}
 : ${LOG_SIZE:="100"}
 : ${CACHE_SIZE:="256"}
 : ${MAX_CLIENT:="250"}
+: ${CHINA_DNS:="127.0.0.1:55,114.114.114.114"}
 
 
-if [ ! -f /usr/bin/dnscrypt ]; then
+if [ ! -f /usr/local/bin/dnscrypt ]; then
 	#bind
 	BIND() {
 		sed -i '/recursion yes;/ a \\n\        \/* #jiobxn.com# *\/' /etc/named.conf
 		sed -i '/#jiobxn.com#/ a \        version  "'"$VERSION"'";' /etc/named.conf
-		[ "$BIND_ADDR" == "0.0.0.0" ] && BIND_ADDR="any"
-		sed -i 's/127.0.0.1;/'$BIND_ADDR';/' /etc/named.conf
+		[ "$LISTEN_ADDR" == "0.0.0.0" ] && LISTEN_ADDR="any"
+		sed -i 's/127.0.0.1;/'$LISTEN_ADDR';/' /etc/named.conf
 		sed -i '/listen-on-v6/d' /etc/named.conf
-		sed -i 's/53/'$BIND_PORT'/' /etc/named.conf
+		[ "$LISTEN_PORT" ] && sed -i 's/53/'$LISTEN_PORT'/' /etc/named.conf
 		sed -i 's/localhost;/any;/' /etc/named.conf
-		[ "$DNS" ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$DNS' };' /etc/named.conf
-		[ "$DNS_ONLY"  ] && sed -i '/#jiobxn.com#/ a \\n\        forward only;' /etc/named.conf
+		[ "$BIND_DNS" ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$BIND_DNS'; };' /etc/named.conf
+		[ "$FORWARD_ONLY"  ] && sed -i '/#jiobxn.com#/ a \\n\        forward only;' /etc/named.conf
 		sed -i '/#jiobxn.com#/ a \\n\        max-cache-size '$CACHE_SIZE'm;' /etc/named.conf
 	
 		if [ "$QUERY_LOG" ]; then
@@ -30,15 +30,15 @@ if [ ! -f /usr/bin/dnscrypt ]; then
 			sed -i '/logging {/ a \        category queries {\n\            query_log;\n\        };' /etc/named.conf
 		fi
 	
-		echo "/usr/sbin/named -u named -c /etc/named.conf -f" >/usr/bin/dnscrypt
+		echo "/usr/sbin/named -u named -c /etc/named.conf -f" >/usr/local/bin/dnscrypt
 	}
 
 
 	#dnscrypt
 	DNSCRYPT() {
-		sed -i "s/'127.0.0.1/'$BIND_ADDR/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		sed -i "s/'127.0.0.1/'$LISTEN_ADDR/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 		sed -i "s/, '\[::1\]:53'//" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
-		sed -i "s/:53']/:$BIND_PORT']/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		[ "$LISTEN_PORT" ] && sed -i "s/:53']/:$LISTEN_PORT']/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 		sed -i 's/cache_size = 256/cache_size = '$CACHE_SIZE'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 		sed -i 's/max_clients = 250/max_clients = '$MAX_CLIENT'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 	
@@ -47,18 +47,24 @@ if [ ! -f /usr/bin/dnscrypt ]; then
 			sed -i 's/log_files_max_size = 10/log_files_max_size = '$LOG_SIZE'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 		fi
 	
-		echo "dnscrypt-proxy" >/usr/bin/dnscrypt
+		echo "dnscrypt-proxy" >/usr/local/bin/dnscrypt
 	}
 
 
 	#init
-	if [ "$DNSCRYPT" ]; then
+	if [ "$CHINADNS" ]; then
+		[ -z "$LISTEN_PORT" ] && LISTEN_PORT=55
+		DNSCRYPT
+		echo "nohup dnscrypt-proxy &" >/usr/local/bin/dnscrypt
+		echo "sleep 50" >>/usr/local/bin/dnscrypt
+		echo "chinadns -m -c /chnroute.txt -s $CHINA_DNS -b $LISTEN_ADDR" >>/usr/local/bin/dnscrypt
+	elif [ "$DNSCRYPT" ]; then
 		DNSCRYPT
 	else
 		BIND
 	fi
 	
-	chmod +x /usr/bin/dnscrypt
+	chmod +x /usr/local/bin/dnscrypt
 fi
 
 	echo "Start ****"
@@ -69,15 +75,17 @@ else
 				docker run -d --restart unless-stopped \\
 				-p 53:53/udp \\
 				-e VERSION=["windows 2003 DNS"] \\
-				-e DNS=<9.9.9.9;8.8.8.8;> \\
-				-e DNS_ONLY=<Y> \\
-				-e BIND_ADDR=[0.0.0.0] \\
-				-e BIND_PORT=[53] \\
+				-e BIND_DNS=<8.8.8.8;9.9.9.9> \\
+				-e FORWARD_ONLY=<Y> \\
+				-e LISTEN_ADDR=[0.0.0.0] \\
+				-e LISTEN_PORT=[53] \\
 				-e MAX_CLIENT=[250] \\
 				-e CACHE_SIZE=[256] \\
 				-e QUERY_LOG=<Y> \\
 				-e LOG_SIZE=[100] \\
 				-e DNSCRYPT=<Y> \\
+				-e CHINADNS=<Y> \\
+				-e CHINA_DNS=[127.0.0.1:55,114.114.114.114] \\
 				--name dns dnscrypt
 	"
 fi
