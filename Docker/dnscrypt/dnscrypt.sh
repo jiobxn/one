d8d49f1fc5788f281affbcb28d0294efa326e2dd
JIOBXN <jiobxn@users.noreply.github.com>
2019-02-10 21:35:08 +0800

    Update dnscrypt.sh

--- Docker/dnscrypt/dnscrypt.sh
+++ Docker/dnscrypt/dnscrypt.sh
@@ -4,49 +4,58 @@ set -e
 if [ "$1" = 'dnscrypt' ]; then
 
 : ${VERSION:="windows 2003 DNS"}
-: ${LOG_SIZE:="100m"}
-: ${LISTEN:="any;"}
-: ${ALLOW_QUERY:="any;"}
-: ${CACHE_SIZE:="100m"}
+: ${BIND_ADDR:="0.0.0.0"}
+: ${BIND_PORT:="53"}
+: ${LOG_SIZE:="100"}
+: ${CACHE_SIZE:="256"}
+: ${MAX_CLIENT:="250"}
 
 
 if [ ! -f /usr/bin/dnscrypt ]; then
 	#bind
-	init_bind() {
+	BIND() {
 		sed -i '/recursion yes;/ a \\n\        \/* #jiobxn.com# *\/' /etc/named.conf
 		sed -i '/#jiobxn.com#/ a \        version  "'"$VERSION"'";' /etc/named.conf
-		sed -i 's/127.0.0.1;/'$LISTEN'/' /etc/named.conf
-		sed -i 's/localhost;/'$ALLOW_QUERY'/' /etc/named.conf
-
-		[ "$FORWARD" ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$FORWARD' };' /etc/named.conf
-		sed -i '/#jiobxn.com#/ a \\n\        max-cache-size '$CACHE_SIZE';' /etc/named.conf
-		
+		[ "$BIND_ADDR" == "0.0.0.0" ] && BIND_ADDR="any"
+		sed -i 's/127.0.0.1;/'$BIND_ADDR';/' /etc/named.conf
+		sed -i '/listen-on-v6/d' /etc/named.conf
+		sed -i 's/53/'$BIND_PORT'/' /etc/named.conf
+		sed -i 's/localhost;/any;/' /etc/named.conf
+		[ "$DNS" ] && sed -i '/#jiobxn.com#/ a \\n\        forwarders   { '$DNS' };' /etc/named.conf
+		[ "$DNS_ONLY"  ] && sed -i '/#jiobxn.com#/ a \\n\        forward only;' /etc/named.conf
+		sed -i '/#jiobxn.com#/ a \\n\        max-cache-size '$CACHE_SIZE'm;' /etc/named.conf
+	
 		if [ "$QUERY_LOG" ]; then
-			sed -i '/logging {/ a \        channel query_log {\n\            file "data/query.log" versions 3 size '$LOG_SIZE';\n\            severity info;\n\            print-time yes;\n\            print-category   yes;\n\        };' /etc/named.conf
+			sed -i '/logging {/ a \        channel query_log {\n\            file "data/query.log" versions 2 size '$LOG_SIZE'm;\n\            severity info;\n\            print-time yes;\n\            print-category   yes;\n\        };' /etc/named.conf
 			sed -i '/logging {/ a \        category queries {\n\            query_log;\n\        };' /etc/named.conf
 		fi
-		
+	
 		echo "/usr/sbin/named -u named -c /etc/named.conf -f" >/usr/bin/dnscrypt
 	}
 
 
 	#dnscrypt
-	init_dnscrypt() {
-		[ "$LISTEN" == "any;" ] && LISTEN="0.0.0.0:53"
-		sed -i 's/127.0.0.1:53/'$(echo $LISTEN)'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+	DNSCRYPT() {
+		sed -i "s/'127.0.0.1/'$BIND_ADDR/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
 		sed -i "s/, '\[::1\]:53'//" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
-		
-		[ "$QUERY_LOG" ] && sed -i "s@# file = 'query.log'@file = '/var/named/data/query.log'@" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
-		sed -i "s/cache_size = 256/cache_size = $(echo $CACHE_SIZE |sed 's/[m|g]//')/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		sed -i "s/:53']/:$BIND_PORT']/" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		sed -i 's/cache_size = 256/cache_size = '$CACHE_SIZE'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		sed -i 's/max_clients = 250/max_clients = '$MAX_CLIENT'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+	
+		if [ "$QUERY_LOG" ]; then
+			sed -i "s@# file = 'query.log'@file = '/var/named/data/query.log'@" /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+			sed -i 's/log_files_max_size = 10/log_files_max_size = '$LOG_SIZE'/' /usr/local/dnscrypt-proxy/dnscrypt-proxy.toml
+		fi
+	
 		echo "dnscrypt-proxy" >/usr/bin/dnscrypt
 	}
 
 
-	#default
+	#init
 	if [ "$DNSCRYPT" ]; then
-		init_dnscrypt
+		DNSCRYPT
 	else
-		init_bind
+		BIND
 	fi
 	
 	chmod +x /usr/bin/dnscrypt
@@ -60,12 +69,14 @@ else
 				docker run -d --restart unless-stopped \\
 				-p 53:53/udp \\
 				-e VERSION=["windows 2003 DNS"] \\
-				-e LISTEN=["any;"] \\
-				-e ALLOW_QUERY=["any;"] \\
-				-e FORWARD=<9.9.9.9;> \\
-				-e CACHE_SIZE=[100m] \\
+				-e DNS=<9.9.9.9;8.8.8.8;> \\
+				-e DNS_ONLY=<Y> \\
+				-e BIND_ADDR=[0.0.0.0] \\
+				-e BIND_PORT=[53] \\
+				-e MAX_CLIENT=[250] \\
+				-e CACHE_SIZE=[256] \\
 				-e QUERY_LOG=<Y> \\
-				-e LOG_SIZE=[100m] \\
+				-e LOG_SIZE=[100] \\
 				-e DNSCRYPT=<Y> \\
 				--name dns dnscrypt
 	"
