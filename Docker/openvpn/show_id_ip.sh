f479a9d90cd18656c10bdd61b9ce530c83f2c838
JIOBXN <jiobxn@users.noreply.github.com>
2019-03-28 10:05:50 +0800

    Update show_id_ip.sh

--- Docker/openvpn/show_id_ip.sh
+++ Docker/openvpn/show_id_ip.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+IP_RANGE=10.8
+NAT_RANGE=10.10.100
+MAX=1023
+
+y1=$(echo $NAT_RANGE |awk -F. '{print $1"."$2}')
+y3=$(echo $NAT_RANGE |awk -F. '{print $3}')
+y4=$(echo $NAT_RANGE |awk -F. '{print $4}')
+[ -z $y3 ] && y3=0
+[ -z $y4 ] && y4=1
+
 i=1
-n=5
-m=6
-while [ $i -le "500" ]; do
-	[ $n -gt 256 ] && n=$(($n-256))
-	[ $m -gt 256 ] && m=$(($m-256))
-	echo "net:$(($i/64)) ip:$n id:$i"
-	n=$(($n+4))
-	m=$(($m+4))
-let i++
+x4=5
+x5=6
+while [ $i -le $MAX ]; do
+	y3=$(($y3+$y4/256))
+	x3=$(($i/64))
+	[ $y4 -eq 256 ] && y4=$(($y4-256))
+	[ $x4 -gt 256 ] && x4=$(($x4-256))
+	#[ $x5 -gt 256 ] && x5=$(($x5-256))
+
+	echo -e "ID: $i   \t IP: $IP_RANGE.$x3.$x4   \t NAT: $y1.$y3.$y4 \t PASS: $(pwmake 64)"
+
+	x4=$(($x4+4))
+	#x5=$(($x5+4))
+	let y4++
+	let i++
 done
