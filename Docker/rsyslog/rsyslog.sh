f51502282df2e09c6d8160edaed6583e5d18c907
JIOBXN <jiobxn@users.noreply.github.com>
2019-05-08 15:23:36 +0800

    Update rsyslog.sh

--- Docker/rsyslog/rsyslog.sh
+++ Docker/rsyslog/rsyslog.sh
@@ -26,14 +26,6 @@ if [ -z "$(grep "redhat.xyz" /etc/rsyslog.d/remote.conf)" ]; then
 	END
 
 	sed -i 's/Listen 80/Listen '$HTTP_PORT'/;/Listen/a ServerName localhost:'$HTTP_PORT'' /etc/httpd/conf/httpd.conf
-
-	ln -s /var/log/rsyslog /var/www/html/down
-	curl -s https://raw.githubusercontent.com/jiobxn/one/master/Docker/rsyslog/index.html >/var/www/html/index.html
-	curl -s https://raw.githubusercontent.com/jiobxn/one/master/Docker/rsyslog/hello.cgi >/var/www/cgi-bin/hello.cgi
-	curl -s https://raw.githubusercontent.com/jiobxn/one/master/Docker/rsyslog/loginfo.sh >/var/www/cgi-bin/day.sh
-	chown apache.apache /var/www/cgi-bin/*
-	chmod 700 /var/www/cgi-bin/*
-
 fi
 
 	echo "Start ****"
