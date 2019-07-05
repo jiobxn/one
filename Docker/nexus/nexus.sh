bff73a1dfe4ed87a65ad2beca93a9dc688c7cf54
JIOBXN <jiobxn@users.noreply.github.com>
2019-07-05 22:21:25 +0800

    Update nexus.sh

--- Docker/nexus/nexus.sh
+++ Docker/nexus/nexus.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 if [ "$4" = 'bin/nexus' ]; then
 
-[ -z "$(ls -ld /usr/local/sonatype-work |egrep -o "nexus nexus")" ] && chown -R nexus.nexus /usr/local/sonatype-work
+[ -z "$(ls -ld /sonatype-work |egrep -o "nexus nexus")" ] && chown -R nexus.nexus /sonatype-work
 
-if [ -z "$(grep redhat.xyz /usr/local/nexus/etc/nexus-default.properties)" ]; then
+if [ -z "$(grep redhat.xyz /nexus/etc/nexus-default.properties)" ]; then
 	\cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-	sed -i '1i #redhat.xyz' /usr/local/nexus/etc/nexus-default.properties
+	sed -i '1i #redhat.xyz' /nexus/etc/nexus-default.properties
 
 	if [ "$RUN_MEM" ]; then
-		sed -i 's/1200M/'$RUN_MEM'/g' /usr/local/nexus/bin/nexus.vmoptions
+		sed -i 's/1200M/'$RUN_MEM'/g' /nexus/bin/nexus.vmoptions
 	fi
 
 	if [ "$MAX_MEM" ]; then
-		sed -i 's/2G/'$MAX_MEM'/' /usr/local/nexus/bin/nexus.vmoptions
+		sed -i 's/2G/'$MAX_MEM'/' /nexus/bin/nexus.vmoptions
 	fi
 	
 	if [ "$NEXUS_PORT" ]; then
-		sed -i 's/8081/'$NEXUS_PORT'/' /usr/local/nexus/etc/nexus-default.properties
+		sed -i 's/8081/'$NEXUS_PORT'/' /nexus/etc/nexus-default.properties
 	fi
 	
 	if [ "$URI_PATH" ]; then
-		sed -i 's#path=/#path='$URI_PATH'#' /usr/local/nexus/etc/nexus-default.properties
+		sed -i 's#path=/#path='$URI_PATH'#' /nexus/etc/nexus-default.properties
 	fi
 fi
 
@@ -32,7 +32,7 @@ else
 	echo -e "
 	Example:
 				docker run -d --restart unless-stopped \\
-				-v /docker/nexus:/usr/local/sonatype-work \\
+				-v /docker/nexus:/sonatype-work \\
 				-p 8081:8081 \\
 				-e RUN_MEM=[1200M] \\
 				-e MAX_MEM=[2G] \\
