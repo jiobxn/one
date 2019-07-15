c5449e367341685862790cb711752634813702fc
JIOBXN <jiobxn@users.noreply.github.com>
2019-07-16 00:36:16 +0800

    Update redis.sh

--- Docker/redis/redis.sh
+++ Docker/redis/redis.sh
@@ -8,36 +8,37 @@ if [ "$1" = 'redis-server' ]; then
 : ${SLAVE_QUORUM:="2"}
 : ${DOWN_TIME:="6000"}
 
-  if [ -z "$(grep "redhat.xyz" /usr/local/redis/redis.conf)" ]; then
+  if [ -z "$(grep "redhat.xyz" /redis/redis.conf)" ]; then
 	echo "Initialize redis"
-	sed -i '1 i #redhat.xyz' /usr/local/redis/redis.conf
+	\cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	sed -i '1 i #redhat.xyz' /redis/redis.conf
 
 	#bind
-	sed -i 's/^bind 127.0.0.1/bind 0.0.0.0/' /usr/local/redis/redis.conf
+	sed -i 's/^bind 127.0.0.1/bind 0.0.0.0/' /redis/redis.conf
 	
 	#port
-	sed -i 's/^port 6379/port '$REDIS_PORT'/' /usr/local/redis/redis.conf
+	sed -i 's/^port 6379/port '$REDIS_PORT'/' /redis/redis.conf
 
 	#persistence
 	if [ $LOCAL_STROGE ]; then
-		sed -i 's@dir \./@dir /usr/local/redis/data@' /usr/local/redis/redis.conf
-		sed -i 's@appendonly no@appendonly yes@' /usr/local/redis/redis.conf
+		sed -i 's@dir \./@dir /redis/data@' /redis/redis.conf
+		sed -i 's@appendonly no@appendonly yes@' /redis/redis.conf
 	fi
 
 	#user auth
 	if [ $REDIS_PASS ]; then
-		echo "requirepass $REDIS_PASS" >>/usr/local/redis/redis.conf
-		echo "Redis password: $REDIS_PASS" |tee /usr/local/redis/data/info
+		echo "requirepass $REDIS_PASS" >>/redis/redis.conf
+		echo "Redis password: $REDIS_PASS" |tee /redis/data/info
 		AUTH="-a $REDIS_PASS"
 	fi
 
 	#redis master
 	if [ $REDIS_MASTER ]; then
-		echo "slaveof $REDIS_MASTER 6379" >>/usr/local/redis/redis.conf
+		echo "slaveof $REDIS_MASTER $REDIS_PORT" >>/redis/redis.conf
 		
 		#sentinel
 		cat >/sentinel.txt<<-END
-		port 26379
+		port $[$REDIS_PORT+10000]
 		dir /tmp
 		protected-mode no
 		sentinel monitor $MASTER_NAME $REDIS_MASTER $REDIS_PORT $SLAVE_QUORUM
@@ -50,12 +51,12 @@ if [ "$1" = 'redis-server' ]; then
 
 	#master pass
 	if [ $MASTER_PASS ]; then
-		echo "masterauth $MASTER_PASS" >>/usr/local/redis/redis.conf
+		echo "masterauth $MASTER_PASS" >>/redis/redis.conf
 		echo "sentinel auth-pass $MASTER_NAME $MASTER_PASS" >>/sentinel.txt
 		
-		if [ -z "$(grep "^requirepass" /usr/local/redis/redis.conf)" ]; then
-			echo "requirepass $MASTER_PASS" >>/usr/local/redis/redis.conf
-			echo "Redis password: $MASTER_PASS" |tee /usr/local/redis/data/info
+		if [ -z "$(grep "^requirepass" /redis/redis.conf)" ]; then
+			echo "requirepass $MASTER_PASS" >>/redis/redis.conf
+			echo "Redis password: $MASTER_PASS" |tee /redis/data/info
 			AUTH="-a $MASTER_PASS"
 		fi
 	fi
@@ -85,37 +86,25 @@ if [ "$1" = 'redis-server' ]; then
 		echo "* * * * * . /etc/profile;/bin/sh /vip.sh &>/dev/null" >>/var/spool/cron/root
 
 		if [ $REDIS_MASTER ]; then
-			\cp /sentinel.txt /usr/local/redis/sentinel.conf
-			echo -e "/usr/local/redis/bin/redis-server /usr/local/redis/redis.conf\n/usr/local/redis/bin/redis-server /usr/local/redis/sentinel.conf --sentinel" >/sentinel.sh
-			sed -i 's/daemonize no/daemonize yes/' /usr/local/redis/redis.conf
+			\cp /sentinel.txt /redis/sentinel.conf
+			echo -e "/redis/bin/redis-server /redis/redis.conf\n/redis/bin/redis-server /redis/sentinel.conf --sentinel" >/sentinel.sh
+			sed -i 's/daemonize no/daemonize yes/' /redis/redis.conf
 		fi
-	fi
-
-	#iptables, Need root authority "--privileged"
-	if [ $IPTABLES ]; then
-		cat > /iptables.sh <<-END
-		iptables -I INPUT -p tcp -m multiport --dport $REDIS_PORT,26379 -j DROP
-		iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport $REDIS_PORT -m comment --comment REDIS -j ACCEPT
-		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport 26379 -m comment --comment REDIS -j ACCEPT
-		END
 	fi
   fi
 
 	echo
 	echo "Start Redis ****"
 	crond
-	[ -f /iptables.sh ] && [ -z "`iptables -S |grep REDIS`" ] && . /iptables.sh
 	[ -f /sentinel.sh ] && . /sentinel.sh
-
 	exec "$@" 1>/dev/null
 
 else
 	echo -e "
 	Example
-					docker run -d --restart always [--privileged] \\
-					-v /docker/redis:/usr/local/redis/data \\
-					-p 6379:6379 \\
+					docker run -d --restart unless-stopped [--privileged] \\
+					-v /docker/redis:/redis/data \\
+					-p 16379:6379 \\
 					-e REDIS_PORT=[6379] \\
 					-e REDIS_PASS=<bigpass> \\
 					-e LOCAL_STROGE=<Y> \\
@@ -125,8 +114,6 @@ else
 					-e MASTER_NAME=[mymaster] \\
 					-e SLAVE_QUORUM=[2] \\
 					-e DOWN_TIME=[6000] \\
-					-e IPTABLES=<"192.168.10.0/24,10.0.0.0/24"> \\
-					--hostname redis \\
 					--name redis redis
 	"
 fi
