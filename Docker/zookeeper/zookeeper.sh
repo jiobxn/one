b5d94c7cd8f24df9a9f5f82452a611354922b061
JIOBXN <jiobxn@users.noreply.github.com>
2019-07-15 23:35:48 +0800

    Update zookeeper.sh

--- Docker/zookeeper/zookeeper.sh
+++ Docker/zookeeper/zookeeper.sh
@@ -2,47 +2,42 @@
 set -e
 
 : ${ZK_MEM:="1G"}
+: ${ZK_APORT:="8080"}
+: ${ZK_CPORT:="2181"}
+: ${ZK_SPORT:="2888"}
 
 if [ "$1" = 'bin/zkServer.sh' ]; then
-  if [ -z "$(grep "redhat.xyz" /usr/local/zookeeper/conf/zoo.cfg 2>/dev/null)" ]; then
+  if [ -z "$(grep "redhat.xyz" /zookeeper/conf/zoo.cfg 2>/dev/null)" ]; then
 	echo "Initialize zookeeper"
-	cp /usr/local/zookeeper/conf/zoo_sample.cfg /usr/local/zookeeper/conf/zoo.cfg
-	sed -i '1 i #redhat.xyz' /usr/local/zookeeper/conf/zoo.cfg
-	sed -i 's#/tmp/zookeeper#/var/lib/zookeeper#' /usr/local/zookeeper/conf/zoo.cfg
-	sed -i 's/#maxClientCnxns=60/maxClientCnxns=0/' /usr/local/zookeeper/conf/zoo.cfg
-	sed -i 's/syncLimit=5/syncLimit=2/' /usr/local/zookeeper/conf/zoo.cfg
-	echo "export JVMFLAGS=\"-Xms$ZK_MEM -Xmx$ZK_MEM \$JVMFLAGS\"" >/usr/local/zookeeper/conf/java.env 
+	\cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	cp /zookeeper/conf/zoo_sample.cfg /zookeeper/conf/zoo.cfg
+	sed -i '1 i #redhat.xyz' /zookeeper/conf/zoo.cfg
+	sed -i 's#/tmp/zookeeper#/zookeeper/data#' /zookeeper/conf/zoo.cfg
+	sed -i 's/#maxClientCnxns=60/maxClientCnxns=0/' /zookeeper/conf/zoo.cfg
+	sed -i 's/syncLimit=5/syncLimit=2/' /zookeeper/conf/zoo.cfg
+	echo "admin.serverPort=$ZK_APORT" >>/zookeeper/conf/zoo.cfg
+	sed -i "s/2181/$ZK_CPORT/" /zookeeper/conf/zoo.cfg
+	echo "export JVMFLAGS=\"-Xms$ZK_MEM -Xmx$ZK_MEM \$JVMFLAGS\"" >/zookeeper/conf/java.env 
 
 	#Clustered
-	if [ $ZK_SERVER ]; then
+	if [ "$ZK_SERVER" ]; then
 		n=0
 		for i in $(echo $ZK_SERVER |sed 's/,/\n/g'); do
 			[ $(echo $ZK_SERVER |sed 's/,/\n/g' |wc -l) -lt 3 ] && echo "nodes is greater than or equal to 3" && exit 1
 			n=$[$n+1]
-			echo "server.$n=$i:2888:3888" >>/usr/local/zookeeper/conf/zoo.cfg
-			[ -n "$(ifconfig |grep $i)" ] && echo $n >/var/lib/zookeeper/myid
+			echo "server.$n=$i:$ZK_SPORT:$[$ZK_SPORT+1000]" >>/zookeeper/conf/zoo.cfg
+			[ -n "$(ifconfig |grep $i)" ] && echo $n >/zookeeper/data/myid
 		done
-		echo "echo stat | nc 127.0.0.1 2181 |awk '\$1==\"Mode:\"{print \$2}'" >/usr/local/bin/zoo
+		echo "echo stat | nc 127.0.0.1 $ZK_CPORT |awk '\$1==\"Mode:\"{print \$2}'" >/usr/local/bin/zoo
 		chmod +x /usr/local/bin/zoo
 	fi
 	
-	#IPTABLES
-	if [ $IPTABLES ]; then
-		cat > /iptables.sh <<-END
-		iptables -I INPUT -p tcp -m multiport --dport 2181 -j DROP
-		iptables -I INPUT -s $IPTABLES -p tcp -m state --state NEW -m tcp --dport 2181 -m comment --comment ZOOKEEPER -j ACCEPT
-		iptables -I INPUT -s -p tcp -m state --state NEW -m tcp --dport 2888 -m comment --comment ZOOKEEPER -j ACCEPT
-		iptables -I INPUT -s -p tcp -m state --state NEW -m tcp --dport 3888 -m comment --comment ZOOKEEPER -j ACCEPT
-		iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-		END
-	fi
-
 	#VIP
-	if [ $VIP ]; then
+	if [ "$VIP" ]; then
 		cat >/vip.sh<<-END
 		#!/bin/bash
 		for i in {1..29}; do
-		if [ -n "\$(netstat -tpnl |grep ":2888 ")" ]; then
+		if [ -n "\$(netstat -tpnl |grep ":$ZK_SPORT ")" ]; then
 		    if [ -z "\$(ifconfig |grep $VIP)" ]; then
 		        ifconfig lo:0 $VIP broadcast $VIP netmask 255.255.255.255 up || echo
 		    fi
@@ -62,21 +57,21 @@ if [ "$1" = 'bin/zkServer.sh' ]; then
 
 	echo "Start ZooKeeper ****"
 	crond
-	[ -f /iptables.sh ] && [ -z "`iptables -S |grep ZOOKEEPER`" ] && . /iptables.sh
 	exec "$@" 1>/dev/null
 
 else
 
     echo -e "
     Example:
-				docker run -d --restart always [--privileged] \\
-				-v /docker/zookeeper:/var/lib/zookeeper \\
+				docker run -d --restart unless-stopped [--privileged] \\
+				-v /docker/zookeeper:/zookeeper/data \\
 				-p 2181:2181 \\
 				-e ZK_MEM=[1G] \\
+				-e ZK_APORT=[8080] \\
+				-e ZK_CPORT=[2181] \\
+				-e ZK_SPORT=[2888] \\
 				-e ZK_SERVER=<"10.0.0.71,10.0.0.72,10.0.0.73"> \\
 				-e VIP=<10.0.0.70> \\
-				-e IPTABLES=<"192.168.10.0/24,10.0.0.0/24"> \\
-				--hostname zookeeper \\
 				--name zookeeper zookeeper
 	"
 fi
