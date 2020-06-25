7460650e696502dd5b94194ac71b792505f555cd
JIOBXN <jiobxn@users.noreply.github.com>
2020-06-25 12:24:23 +0800

    Update chnroute.sh

--- Docker/chnroute/chnroute.sh
+++ Docker/chnroute/chnroute.sh
@@ -3,6 +3,9 @@ set -e
 
 : ${LOCAL_GW:="172.17.0.1"}
 : ${UP_TIME:="daily"}
+: ${DEV:="$(ip route |awk '$1=="0.0.0.0/1"{print $NF}')"}
+: ${RAN:="$(iptables -t nat -S |awk '$NF=="MASQUERADE"{print $4}')"}
+
 
 if [ "$1" = 'crond' ]; then
 
@@ -21,6 +24,7 @@ if [ "$1" = 'crond' ]; then
         chmod +x /etc/periodic/$UP_TIME/route.sh
 
         echo "awk '{print \"ip route add\",\$1,\"via $LOCAL_GW\"}' /chnroute.txt |sh 2>/dev/null || echo" >/usr/local/bin/chnroute
+        [ "$SNAT" ] && echo "iptables -t nat -I POSTROUTING -s $RAN -o $DEV -j MASQUERADE" >>/usr/local/bin/chnroute
         chmod +x /usr/local/bin/chnroute
     fi
 
@@ -36,5 +40,8 @@ else
 				-e LOCAL_GW=[172.17.0.0.1] \\
 				-e LOCAL_ROUTE=<192.168.0.0/24,10.10.0.0/16> \\
 				-e UP_TIME=[daily] \\
+				-e SNAT=<Y> \\
+				-e DEV=<default> \\
+				-e RAN=<default> \\
 				--name chnroute jiobxn/chnroute
 fi
