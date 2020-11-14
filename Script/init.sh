ca9d299ce6accb3458ba73623c7ed85c98f75dba
JIOBXN <jiobxn@users.noreply.github.com>
2020-11-14 10:59:50 +0800

    Update init.sh

--- Script/init.sh
+++ Script/init.sh
@@ -15,7 +15,7 @@ sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 
 [ -z "$(grep termencoding /etc/vimrc)" ] && echo -e "set fileencodings=utf-8,ucs-bom,gb18030,gbk,gb2312,cp936\nset termencoding=utf-8\nset encoding=utf-8" >>/etc/vimrc
 
-wget https://github.com/jiobxn/one/raw/master/Script/scan.sh -O /usr/local/sbin/scan.sh
+wget https://github.com/jiobxn/one/raw/master/Script/scan2.sh -O /usr/local/sbin/scan.sh
 chmod u+x /usr/local/sbin/scan.sh
 
 cat >/var/spool/cron/root <<-EOF
