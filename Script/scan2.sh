8828b1b5b9c9a55dd7c7d2972fb4b4201026a33e
JIOBXN <jiobxn@users.noreply.github.com>
2020-11-14 10:58:41 +0800

    Create scan2.sh

---
+++ Script/scan2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# ipset init
+ipset_lock() {
+	[ -z "`ipset list blacklist 2>/dev/null |grep -w blacklist`" ] && ipset create blacklist hash:net maxelem 1000000
+	[ -z "`ipset list whitelist 2>/dev/null |grep -w whitelist`" ] && ipset create whitelist hash:net maxelem 1000000
+	[ -z "`iptables -S |grep -w blacklist`" ] && iptables -I INPUT -m set --match-set blacklist src -j DROP
+	[ -z "`iptables -S |grep -w whitelist`" ] && iptables -I INPUT -m set --match-set whitelist src -j ACCEPT
+	[ -z "`iptables -S |grep -w SSH-SYN`" ] && iptables -I INPUT -p tcp --dport 22 --syn -m state --state NEW -m recent --name SSH-SYN --update --seconds 60 --hitcount 5 -j DROP
+  
+	touch /tmp/.ipset.lock
+	
+	if [ -f /var/log/ipset.list ]; then
+		[ "$(ipset list |egrep ^[0-9] |wc -l)" -lt "$(wc -l /var/log/ipset.list |awk '{print $1}')" ] && ipset -R < /var/log/ipset.list
+	fi
+}
+
+[ ! -f /tmp/.ipset.lock ] && ipset_lock
+
+
+# ssh log
+SSH=$(egrep "Failed" /var/log/secure |awk '{print $(NF-3)}' |grep ^[1-9] |sort |uniq -c |awk '$1>10' |awk '{print $1"="$2}')  
+#SSH=$(lastb |awk '$3~"^[1-9]"{print $3}' |sort |uniq -c |awk '$1>10' |awk '{print $1"="$2}')
+
+
+# drop
+for i in $SSH; do
+    NUMBER=`echo $i |awk -F= '{print $1}'`   
+    SCANIP=`echo $i |awk -F= '{print $2}'`    
+
+    if [ -n "$(ipset test blacklist $SCANIP 2>&1 |grep -w NOT)" ]; then
+        echo "$SCANIP($NUMBER)" 
+        ipset add blacklist $SCANIP
+        echo "add blacklist $SCANIP" >>/var/log/ipset.list
+        echo "`date +%-F/%-H:%-M:%-S` $SCANIP ($NUMBER) $ADDRESS" >> /var/log/scanip.log
+    fi
+done
