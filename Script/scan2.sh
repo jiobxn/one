9c42f9d1f66c27220577604c38342faf90d3d093
JIOBXN <jiobxn@users.noreply.github.com>
2020-11-18 10:39:11 +0800

    Update scan2.sh

--- Script/scan2.sh
+++ Script/scan2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# * * * * * . /etc/profile; bash /usr/local/sbin/scan2.sh
 # 0 0 * * * cp /var/log/secure /var/log/secure.$(date -d "-1 day" +\%F) ; > /var/log/secure
 # echo "\rm /tmp/.ipset.lock" >> /etc/rc.local
 
