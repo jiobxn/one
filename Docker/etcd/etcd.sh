998ac00bded5a766040e283503284b0ea322fae4
JIOBXN <jiobxn@users.noreply.github.com>
2021-02-27 19:21:38 +0800

    best

--- Docker/etcd/etcd.sh
+++ Docker/etcd/etcd.sh
@@ -6,7 +6,6 @@ if [ "$1" = 'ETCD' ]; then
 : ${ETCD_PORT:="2379"}
 : ${ETCD_TOKEN:="token-01"}
 : ${CLUSTER_STATE:="new"}
-: ${ETCD_COUNT:="10000"}
 
   if [ ! -f /usr/local/bin/ETCD ]; then
 	echo "Initialize redis"
@@ -28,35 +27,25 @@ if [ "$1" = 'ETCD' ]; then
 			fi
 			n=$(($n+1))
 		done
+		
+		CLUSTER="$(cat /cluster.txt)"
+		echo "etcd --data-dir=/data/etcd --name=${THIS_NAME} --initial-advertise-peer-urls=http://${THIS_IP}:${THIS_PORT} --listen-peer-urls=http://${THIS_IP}:${THIS_PORT} --advertise-client-urls=http://${THIS_IP}:${ETCD_PORT} --listen-client-urls=http://127.0.0.1:${ETCD_PORT},http://${THIS_IP}:${ETCD_PORT} --initial-cluster=${CLUSTER} --initial-cluster-state=${CLUSTER_STATE} --initial-cluster-token=${ETCD_TOKEN}" > /usr/local/bin/ETCD
 	else
 		THIS_IP="$(ip route |awk '$3=="'$(ip route |awk '$1=="default"{print $5}')'" && $NR1~"src"' |awk -F' src ' '{print $2}' |awk '{print $1}')"
-		THIS_PORT="2380"
 		THIS_NAME="etcd"
-		echo "etcd=http://$THIS_IP:2380" >/cluster.txt
+		echo "etcd --data-dir=/data/etcd --name=${THIS_NAME} --advertise-client-urls=http://${THIS_IP}:${ETCD_PORT} --listen-client-urls=http://127.0.0.1:${ETCD_PORT},http://${THIS_IP}:${ETCD_PORT}" > /usr/local/bin/ETCD
 	fi
-	
-	CLUSTER="$(cat /cluster.txt)"
 
-	# 
-    if [ ! -d /data/etcd ]; then
-		echo "etcd --data-dir=/data/etcd --name=${THIS_NAME} --initial-advertise-peer-urls=http://${THIS_IP}:${THIS_PORT} --listen-peer-urls=http://${THIS_IP}:${THIS_PORT} --advertise-client-urls=http://${THIS_IP}:${ETCD_PORT} --listen-client-urls=http://127.0.0.1:${ETCD_PORT},http://${THIS_IP}:${ETCD_PORT} --initial-cluster=${CLUSTER} --initial-cluster-state=${CLUSTER_STATE} --initial-cluster-token=${ETCD_TOKEN} --snapshot-count=${ETCD_COUNT}" > /usr/local/bin/ETCD
-		chmod +x /usr/local/bin/ETCD
-	else
-		echo "/data/etcd exists, skip.."
-	fi
-	
+	chmod +x /usr/local/bin/ETCD
+
+
 	# TLS
 	if [ "$AUTO_TLS" ]; then
 		sed -i 's/$/ --auto-tls --peer-auto-tls/' /usr/local/bin/ETCD
 		sed -i 's/http:/https:/g' /usr/local/bin/ETCD
-		sed -i 's/client-urls=https/client-urls=http/g' /usr/local/bin/ETCD
-		sed -i 's#client-urls=http://127.0.0.1:2379,https#client-urls=http://127.0.0.1:2379,http#' /usr/local/bin/ETCD
+		echo "https://$THIS_IP:$ETCD_PORT"
 	elif [ -f /key/ca.crt -a -f /key/server.crt -a -f /key/server.key -a -f /key/peer.crt -a -f /key/peer.key ]; then
 		sed -i 's#$# --cert-file=/key/server.crt --client-cert-auth=true --key-file=/key/server.key --peer-cert-file=/key/peer.crt --peer-client-cert-auth=true --peer-key-file=/key/peer.key --peer-trusted-ca-file=/key/ca.crt --trusted-ca-file=/key/ca.crt#' /usr/local/bin/ETCD
-		sed -i 's/http:/https:/g' /usr/local/bin/ETCD
-		echo "https://$THIS_IP:$ETCD_PORT"
-	elif [ -f /key/ca.crt -a -f /key/peer.crt -a -f /key/peer.key ]; then
-		sed -i 's#$# --peer-client-cert-auth --peer-trusted-ca-file=/key/ca.crt --peer-cert-file=/key/peer.crt --peer-key-file=/key/peer.key#' /usr/local/bin/ETCD
 		sed -i 's/http:/https:/g' /usr/local/bin/ETCD
 		echo "https://$THIS_IP:$ETCD_PORT"
 	elif [ -f /key/ca.crt -a -f /key/server.crt -a -f /key/server.key ]; then
@@ -64,7 +53,7 @@ if [ "$1" = 'ETCD' ]; then
 		sed -i 's/http:/https:/g' /usr/local/bin/ETCD
 		echo "https://$THIS_IP:$ETCD_PORT"
 	elif [ -f /key/server.crt -a -f /key/server.key ]; then
-		sed -i 's#$# --peer-cert-file=/key/server.crt --peer-key-file=/key/server.key#' /usr/local/bin/ETCD
+		sed -i 's#$# --cert-file=/key/server.crt --key-file=/key/server.key#' /usr/local/bin/ETCD
 		sed -i 's/http:/https:/g' /usr/local/bin/ETCD
 		echo "https://$THIS_IP:$ETCD_PORT"
 	else
@@ -89,7 +78,6 @@ else
 					-e ETCD_PORT=[2379] \\
 					-e ETCD_TOKEN=[token-01] \\
 					-e CLUSTER_STATE=[new] \\
-					-e ETCD_COUNT=[10000] \\
 					-e CLUSTER=<10.0.0.11:2380,10.0.0.12:2380,10.0.0.13:2380> \\
 					-e AUTO_TLS=<Y> \\
 					--name etcd etcd
